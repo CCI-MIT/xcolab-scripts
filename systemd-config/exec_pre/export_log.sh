3245af371f581918d1cdef57c7cf1b9256093dc2
Miguel <gamallomiguel@gmail.com>
2018-08-13 15:44:24 -0400

    Adjust export for previous execution, not current one

--- systemd-config/exec_pre/export_log.sh
+++ systemd-config/exec_pre/export_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Output service log only from last execution, or from the beginning if pattern is not found
-lastGrepLineNumber=$(/usr/bin/journalctl -u $1 | grep -n "Starting $2 Service" | cut -f1 -d: | tail -1)
+lastGrepLineNumber=$(/usr/bin/journalctl -u $1 | grep -n "Starting $2 Service" | cut -f1 -d: | tac | sed -n 2p)
 if [ -z $lastGrepLineNumber ]; then
 	echo "$(/usr/bin/journalctl -u $1)"
 else
