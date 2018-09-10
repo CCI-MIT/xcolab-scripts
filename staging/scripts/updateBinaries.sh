628c54f8e22cb4ce94f62f61da70c2017d5e1438
Johannes Bachhuber <6458867+jobachhu@users.noreply.github.com>
2018-09-10 15:57:02 +0200

    Update binary copy script

--- staging/scripts/updateBinaries.sh
+++ staging/scripts/updateBinaries.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-scp -r travis@cognosis.mit.edu:homolog/binaries ~/
-#scp -r binaries@cognosis2.mit.edu:xcolab/homolog/binaries ~/
+scp -r binaries@cognosis2.mit.edu:xcolab/homolog/binaries ~/
